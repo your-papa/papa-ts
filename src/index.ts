ff80dae45329b5530f3984e4172aaede4b7611a2
Leo310 <leodev310@gmail.com>
2024-04-05 18:06:14 +0200

    refactor: exposed interfaces

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,6 @@ import { Papa, PapaData, PapaResponseStatus } from './Papa';
 import { obsidianDocumentLoader } from './ObsidianDocumentLoader';
 import { Language, Languages, Prompts } from './Prompts';
 import { LogLvl } from './Logging';
-import { OpenAIGenModel, OpenAIEmbedModel, OllamaGenModel, OllamaEmbedModel, GenModel } from './Models';
+import { GenModel, EmbedModel } from './Models';
 
-export {
-    Papa,
-    PapaData,
-    PapaResponseStatus,
-    obsidianDocumentLoader,
-    Prompts,
-    Language,
-    Languages,
-    LogLvl,
-    GenModel,
-    OpenAIGenModel,
-    OpenAIEmbedModel,
-    OllamaGenModel,
-    OllamaEmbedModel,
-};
+export { Papa, PapaData, PapaResponseStatus, obsidianDocumentLoader, Prompts, Language, Languages, LogLvl, EmbedModel, GenModel };
