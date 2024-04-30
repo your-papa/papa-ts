73975bc4035dd678a85a949070d609702a120787
Leo310 <leodev310@gmail.com>
2024-04-30 12:31:36 +0200

    Revert "refactor: exposed interfaces"
    This reverts commit ff80dae45329b5530f3984e4172aaede4b7611a2.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,20 @@ import { Papa, PapaData, PapaResponseStatus } from './Papa';
 import { obsidianDocumentLoader } from './ObsidianDocumentLoader';
 import { Language, Languages, Prompts } from './Prompts';
 import { LogLvl } from './Logging';
-import { GenModel, EmbedModel } from './Models';
+import { OpenAIGenModel, OpenAIEmbedModel, OllamaGenModel, OllamaEmbedModel, GenModel } from './Models';
 
-export { Papa, PapaData, PapaResponseStatus, obsidianDocumentLoader, Prompts, Language, Languages, LogLvl, EmbedModel, GenModel };
+export {
+    Papa,
+    PapaData,
+    PapaResponseStatus,
+    obsidianDocumentLoader,
+    Prompts,
+    Language,
+    Languages,
+    LogLvl,
+    GenModel,
+    OpenAIGenModel,
+    OpenAIEmbedModel,
+    OllamaGenModel,
+    OllamaEmbedModel,
+};
